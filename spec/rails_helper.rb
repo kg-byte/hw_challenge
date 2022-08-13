6fcf089cf82b69c665e0232717f1c1e282cf1957
Kim G <xiaole.guo@yahoo.com>
2022-08-13 14:41:17 -0600

    ignore hw_schema and mutation_type in coverage percent calculation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do 
+  add_filter 'app/graphql/hw_challenge_schema.rb'
+  add_filter 'app/graphql/types/mutation_type.rb'
+end
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
